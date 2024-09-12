133bd4998066a26eaed273438c249361795de39d
Dmitrii Fedorov <dmitriyfed762@mail.ru>
2024-09-12 18:27:55 +0300

    core functioning

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,120 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type MetricType string
+
+const (
+	Gauge   MetricType = "gauge"
+	Counter MetricType = "counter"
+)
+
+type MetricStorage interface {
+	UpdateGauge(name string, value float64)
+	UpdateCounter(name string, value int64)
+	GetMetrics() map[string]interface{}
+}
+
+type MemStorage struct {
+	gauges   map[string]float64
+	counters map[string]int64
+	mu       sync.RWMutex
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]float64),
+		counters: make(map[string]int64),
+	}
+}
+
+func (ms *MemStorage) UpdateGauge(name string, value float64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	ms.gauges[name] = value
+}
+
+func (ms *MemStorage) UpdateCounter(name string, value int64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	ms.counters[name] += value
+}
+
+func (ms *MemStorage) GetMetrics() map[string]interface{} {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
+	metrics := make(map[string]interface{})
+	for k, v := range ms.gauges {
+		metrics[k] = v
+	}
+
+	for k, v := range ms.counters {
+		metrics[k] = v
+	}
+
+	return metrics
+}
+
+func UpdateMetricHandler(storage MetricStorage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 5 {
+			http.Error(w, "metric ID required", http.StatusNotFound)
+			return
+		}
+
+		metricType := parts[2]
+		metricName := parts[3]
+		metricValue := parts[4]
+
+		switch metricType {
+		case "gauge":
+			value, err := strconv.ParseFloat(metricValue, 64)
+			if err != nil {
+				http.Error(w, "invalid gauge value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateGauge(metricName, value)
+
+		case "counter":
+			value, err := strconv.ParseInt(metricValue, 10, 64)
+			if err != nil {
+				http.Error(w, "invalid counter value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateCounter(metricName, value)
+
+		default:
+			http.Error(w, "invalid metric type", http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Metric updated successefully")
+	}
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "resource not found", http.StatusNotFound)
+}
+
+func main() {
+	serveMux := http.NewServeMux()
+	memStorage := NewMemStorage()
+
+	serveMux.HandleFunc("/update/", UpdateMetricHandler(memStorage))
+	serveMux.HandleFunc("/", NotFoundHandler)
+
+	log.Println("starting server on :8080")
+	if err := http.ListenAndServe(":8080", serveMux); err != nil {
+		log.Fatalf("Error occured starting server: %v", err)
+	}
+}
