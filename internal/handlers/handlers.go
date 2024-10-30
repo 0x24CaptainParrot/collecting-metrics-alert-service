51a9441ef86aec644db7ede2f8afab5990482fb6
Dmitrii Fedorov <dmitriyfed762@mail.ru>
2024-10-30 20:26:06 +0300

    error checking in handlers added

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/0x24CaptainParrot/collecting-metrics-alert-service.git/internal/storage"
 	"github.com/go-chi/chi/v5"
@@ -20,12 +19,12 @@ func NewHandler(storage storage.MetricStorage) *Handler {
 }
 
 func (h *Handler) UpdateMetricHandler(w http.ResponseWriter, r *http.Request) {
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) != 5 {
-		http.Error(w, "metric ID required", http.StatusBadRequest)
-		log.Printf("Metric id was not given")
-		return
-	}
+	// parts := strings.Split(r.URL.Path, "/")
+	// if len(parts) != 5 {
+	// 	http.Error(w, "metric ID required", http.StatusBadRequest)
+	// 	log.Printf("Metric id was not given")
+	// 	return
+	// }
 
 	metricType := chi.URLParam(r, "type")
 	if storage.MetricType(metricType) != storage.Gauge && storage.MetricType(metricType) != storage.Counter {
@@ -34,6 +33,12 @@ func (h *Handler) UpdateMetricHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	metricName := chi.URLParam(r, "name")
 	metricValue := chi.URLParam(r, "value")
+
+	if metricType == "" || metricName == "" || metricValue == "" {
+		http.Error(w, "missing metric ID or value", http.StatusBadRequest)
+		log.Printf("Metric ID, type or value is missing in the request")
+		return
+	}
 
 	switch storage.MetricType(metricType) {
 	case storage.Gauge:
@@ -67,12 +72,21 @@ func (h *Handler) GetMetricValueHandler(w http.ResponseWriter, r *http.Request) 
 	metricType := storage.MetricType(chi.URLParam(r, "type"))
 	metricName := chi.URLParam(r, "name")
 
+	if metricType == "" || metricName == "" {
+		http.Error(w, "missing metric type or name", http.StatusBadRequest)
+		log.Printf("Metric type or name is missing in the request")
+		return
+	}
+
 	metric, err := h.storage.GetMetric(metricName, metricType)
 	if err != nil {
 		http.Error(w, "metric not found", http.StatusNotFound)
 		log.Printf("Metric not found: %s %s", metricType, metricName)
 		return
 	}
+
+	w.Header().Set("Content-Type", "text/json")
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%v", metric)
 }
 
