e0c043a45ab3f0c40b60843e3239a570f80c1968
Dmitrii Fedorov <dmitriyfed762@mail.ru>
2024-10-30 19:30:51 +0300

    scheme changed

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -9,7 +9,7 @@ var agentConfig struct {
 }
 
 func parseAgentFlags() {
-	flag.StringVar(&agentConfig.endpointAddr, "a", "http://localhost:8080", "endpoint addr of the server")
+	flag.StringVar(&agentConfig.endpointAddr, "a", "http://127.0.0.1:8080", "endpoint addr of the server")
 	flag.IntVar(&agentConfig.reportInterval, "r", 10, "report interval")
 	flag.IntVar(&agentConfig.pollInterval, "p", 2, "poll interval")
 
