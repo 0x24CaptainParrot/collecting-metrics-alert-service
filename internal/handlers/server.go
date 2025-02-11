3772690753f2c83708f1c91b90f6a7e33c3f6582
Dmitrii Fedorov <dmitriyfed762@mail.ru>
2025-02-11 17:33:21 +0300

    routes changed

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -38,9 +38,9 @@ func NewRouter(service *service.Service) http.Handler {
 	r.Use(middlewares.GzipMiddleware)
 
 	h := NewHandler(service)
-	// r.Post("/update/", h.UpdateMetricJSONHandler)
+	r.Post("/update/", h.UpdateMetricJSONHandler)
 	r.Post("/update", h.UpdateMetricJSONHandler)
-	// r.Post("/value/", h.GetMetricJSONHandler)
+	r.Post("/value/", h.GetMetricJSONHandler)
 	r.Post("/value", h.GetMetricJSONHandler)
 	r.Post("/update/{type}/{name}/{value}", h.UpdateMetricHandler)
 	r.Get("/value/{type}/{name}", h.GetMetricValueHandler)
